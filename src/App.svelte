882d5eaf49a323331cb4fe68a84b9e028bb5256c
Luis C. Gómez <46666572+chapig@users.noreply.github.com>
2021-07-26 07:47:28 -0400

    Projects routes added
    Using project component to list projects and services in the services route.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script>
-	import { Router, Route, Link } from "svelte-navigator";
+	import { Router, Route } from "svelte-navigator";
 	import Header from "./components/Header.svelte";
 	import Footer from "./components/Footer.svelte";
 	import TutoringAd from "./components/TutoringAd.svelte";
-	import Projects from "./components/Project/Project.svelte";
 	import Hero from "./components/Hero.svelte";
 	import Project from "./components/Project/Project.svelte";
 </script>
@@ -30,16 +29,85 @@
 		<Header />
 	</header>
 	
-	<main>
+	<main class="bg-white">
 		
 		<Route path="/">
 			<Hero />
-			<Project title="Hola" subtitle="Hola" github="a" tag="a" />
 			<TutoringAd />
 		</Route>
 
 		<Route path="services">
-			<Projects />
+
+			<h1 class="pt-16 mb-4 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight text-center bg-white">
+                <span>Estos son mis</span>  <span class="lg:block md:block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-yellow-600 to-yellow-900">proyectos.</span>
+            </h1>
+
+			<section class="py-6 lg:py-12 lg:flex lg:justify-center">
+				<Project
+					backgroundcolor = "bg-gray-100"
+					title="Alfonso"
+					titlecolor = "text-black"
+					subtitle="Bot para Discord"
+					subtitlecolor = "text-gray-900"
+					tag="Python"
+					tagcolor = "bg-yellow-300"
+					tagtextcolor = "text-black"
+					description="Bot usando la API de Discord escrita en Python, soporta distintos prefijos para
+					cada servidor en que el bot se encuentre, reproducción de música, calculadora
+					de matemáticas, precio de monedas fiat y criptomonedas, búsqueda en
+					Wikipedia, juegos y comandos de ocio."
+					descriptioncolor = "text-black"
+					github = "https://www.github.com/chapig/alfonso"
+					githubcolor = "text-black"
+				/>
+
+				<Project
+					backgroundcolor = "bg-gray-100"
+					title="Base de datos para tutoría"
+					titlecolor = "text-black"
+					subtitle="Puente de bases de datos"
+					subtitlecolor = "text-gray-900"
+					tag="Python & SQLite"
+					tagcolor = "bg-gradient-to-r from-yellow-300 to-blue-500"
+					tagtextcolor = "text-black"
+					description="Puente para controlar datos de la base de datos, que contiene fecha de pago, monto, deuda, código de estudiante, nacionalidad y país de residencia."
+					descriptioncolor = "text-black"
+					github = "https://www.github.com/chapig/tutoringdb"
+					githubcolor = "text-black"
+				/>
+
+				<Project
+				backgroundcolor = "bg-gray-100"
+				title="Mathenize"
+				titlecolor = "text-black"
+				subtitle="Parser de operaciones matemáticas"
+				subtitlecolor = "text-gray-900"
+				tag="Julia"
+				tagcolor = "bg-pink-500"
+				tagtextcolor = "text-white"
+				description="Realiza operaciones matemáticas desde strings usando el lenguaje de
+				programación Julia, la mayoría de las funciones matemáticas y expresiones son
+				compatibles, incluyendo matrices y operaciones con paréntesis."
+				descriptioncolor = "text-black"
+				github = "https://www.github.com/chapig/mathenizejl"
+				githubcolor = "text-black"
+				/>
+
+				<Project
+					backgroundcolor = "bg-gray-100"
+					title="chapig.com"
+					titlecolor = "text-black"
+					subtitle="Sitio web"
+					subtitlecolor = "text-gray-900"
+					tag="Svelte"
+					tagcolor = "bg-red-700"
+					tagtextcolor = "text-white"
+					description="Sitio web que muestra el currículum de Luis C. Gómez, sus servicios y proyectos."
+					descriptioncolor = "text-black"
+					github = "https://www.github.com/chapig/website"
+					githubcolor = "text-black"
+				/>
+			</section>
 		</Route>
 
 		<Route path="cv">
