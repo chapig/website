276d4fb9bef4bc81b3355bba7eb88b950c74c79c
Luis C. Gómez <46666572+chapig@users.noreply.github.com>
2021-07-26 07:44:36 -0400

    Padding and margin update

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,4 +1,4 @@
-<section class="pt-10 lg:pt-24 bg-white">
+<section class="pt-28 lg:pt-24 bg-white">
     <div class="px-12 mx-auto max-w-7xl">
         <div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
             <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
@@ -15,7 +15,7 @@
                     </div>
                 </div>
 
-            <div class="mb-4 space-x-0 md:space-x-2 md:mb-8">
+            <div class="space-x-0 md:space-x-2 md:mb-8">
                 <a href="#_" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-black bg-yellow-400 rounded-md sm:w-auto sm:mb-0 hover:bg-gray-800 hover:text-white transition duration-200">
                     Currículum
                     <svg class="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
