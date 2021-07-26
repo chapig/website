de4059d0b1a2c3b969c15df116ca53b47a95b819
Luis C. Gómez <46666572+chapig@users.noreply.github.com>
2021-07-26 07:45:44 -0400

    Addes svelte-navigator
    Button now able to access new routes.

--- src/components/TutoringAd.svelte
+++ src/components/TutoringAd.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { Link } from "svelte-navigator";
     let classinfo = 'Imparto clases personalizadas de francés e inglés de <span class="font-bold">lunes a viernes</span> a través de internet.'
     let projectideas = 'Échale un vistazo a mis proyectos de software e ideas que planeo realizar en un futuro cercano.'
 </script>
@@ -12,9 +13,8 @@
         <div class="max-w-xl px-6 py-12 lg:max-w-5xl lg:w-1/2">
             <h2 class="text-2xl font-bold text-black dark:text-white md:text-3xl">Inscríbete en mis clases privadas de francés o inglés</h2>
             <p class="mt-4 text-black dark:text-gray-400"> {@html classinfo} </p>
-            
             <div class="mt-8">
-                <a href="a" class="px-5 py-2 text-md text-black transition-colors duration-200 transform bg-yellow-400 rounded-md hover:bg-gray-800 hover:text-white hover:font-medium">Inscríbete</a>
+                <button class="px-5 py-2 text-md text-black transition-colors duration-200 transform bg-yellow-400 rounded-md hover:bg-gray-800 hover:text-white hover:font-medium">Inscríbete</button>
             </div>
         </div>
     </div>
@@ -28,7 +28,7 @@
             <h2 class="text-2xl font-bold text-black dark:text-white md:text-3xl">Conoce mis proyectos, ideas e initicativas</h2>
             <p class="mt-4 text-gray-900 dark:text-gray-400 font-bold"> {@html projectideas} </p>
             <div class="mt-8">
-                <a href="a" class="px-5 py-2 text-md text-black transition-colors duration-200 transform bg-white rounded-md hover:bg-gray-800 hover:text-white hover:font-medium">Más información</a>
+                <Link to="services" class="px-5 py-2 text-md text-black transition-colors duration-200 transform bg-white rounded-md hover:bg-gray-800 hover:text-white hover:font-medium">Más información</Link>
             </div>
         </div>
     </div>
