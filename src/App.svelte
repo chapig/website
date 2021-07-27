05a9c68b15e450a607831be8efe588a78a201ddb
Luis C. Gómez <46666572+chapig@users.noreply.github.com>
2021-07-26 21:03:57 -0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,8 @@
 	import TutoringAd from "./components/TutoringAd.svelte";
 	import Hero from "./components/Hero.svelte";
 	import Project from "./components/Project/Project.svelte";
+	import Services from "./components/Services.svelte";
+	import { fly, fade } from 'svelte/transition';
 </script>
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -24,93 +26,100 @@
 			font-family: 'Inter', sans-serif;
 		}
 </style>
+
 <Router>
 	<header>
 		<Header />
 	</header>
 	
-	<main class="bg-white">
+	<main>
 		
 		<Route path="/">
-			<Hero />
-			<TutoringAd />
+			<div in:fly>
+				<Hero />
+				<TutoringAd />
+			</div>
+		</Route>
+
+		<Route path="projects">
+			<div in:fly>
+					<h1 class="pt-16 mb-4 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight text-center">
+						<span>Estos son mis</span>  <span class="lg:block md:block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-yellow-600 to-yellow-900">proyectos.</span>
+					</h1>
+
+					<section class="py-6 lg:py-12 lg:flex lg:justify-center">
+						<Project
+							backgroundcolor = "bg-gray-100"
+							title="Alfonso"
+							titlecolor = "text-black"
+							subtitle="Bot para Discord"
+							subtitlecolor = "text-gray-900"
+							tag="Python"
+							tagcolor = "bg-yellow-300"
+							tagtextcolor = "text-black"
+							description="Bot usando la API de Discord escrita en Python, soporta distintos prefijos para
+							cada servidor en que el bot se encuentre, reproducción de música, calculadora
+							de matemáticas, precio de monedas fiat y criptomonedas, búsqueda en
+							Wikipedia, juegos y comandos de ocio."
+							descriptioncolor = "text-black"
+							github = "https://www.github.com/chapig/alfonso"
+							githubcolor = "text-black"
+						/>
+
+						<Project
+							backgroundcolor = "bg-gray-100"
+							title="Base de datos para tutoría"
+							titlecolor = "text-black"
+							subtitle="Puente de bases de datos"
+							subtitlecolor = "text-gray-900"
+							tag="Python & SQLite"
+							tagcolor = "bg-gradient-to-r from-yellow-300 to-blue-500"
+							tagtextcolor = "text-black"
+							description="Puente para controlar datos de la base de datos, que contiene fecha de pago, monto, deuda, código de estudiante, nacionalidad y país de residencia."
+							descriptioncolor = "text-black"
+							github = "https://www.github.com/chapig/tutoringdb"
+							githubcolor = "text-black"
+						/>
+
+						<Project
+						backgroundcolor = "bg-gray-100"
+						title="Mathenize"
+						titlecolor = "text-black"
+						subtitle="Parser de operaciones matemáticas"
+						subtitlecolor = "text-gray-900"
+						tag="Julia"
+						tagcolor = "bg-pink-500"
+						tagtextcolor = "text-white"
+						description="Realiza operaciones matemáticas desde strings usando el lenguaje de
+						programación Julia, la mayoría de las funciones matemáticas y expresiones son
+						compatibles, incluyendo matrices y operaciones con paréntesis."
+						descriptioncolor = "text-black"
+						github = "https://www.github.com/chapig/mathenizejl"
+						githubcolor = "text-black"
+						/>
+
+						<Project
+							backgroundcolor = "bg-gray-100"
+							title="chapig.com"
+							titlecolor = "text-black"
+							subtitle="Sitio web"
+							subtitlecolor = "text-gray-900"
+							tag="Svelte"
+							tagcolor = "bg-red-700"
+							tagtextcolor = "text-white"
+							description="Sitio web que muestra el currículum de Luis C. Gómez, sus servicios y proyectos."
+							descriptioncolor = "text-black"
+							github = "https://www.github.com/chapig/website"
+							githubcolor = "text-black"
+						/>
+					</section>
+				</div>
 		</Route>
 
 		<Route path="services">
-
-			<h1 class="pt-16 mb-4 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight text-center bg-white">
-                <span>Estos son mis</span>  <span class="lg:block md:block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-yellow-600 to-yellow-900">proyectos.</span>
-            </h1>
-
-			<section class="py-6 lg:py-12 lg:flex lg:justify-center">
-				<Project
-					backgroundcolor = "bg-gray-100"
-					title="Alfonso"
-					titlecolor = "text-black"
-					subtitle="Bot para Discord"
-					subtitlecolor = "text-gray-900"
-					tag="Python"
-					tagcolor = "bg-yellow-300"
-					tagtextcolor = "text-black"
-					description="Bot usando la API de Discord escrita en Python, soporta distintos prefijos para
-					cada servidor en que el bot se encuentre, reproducción de música, calculadora
-					de matemáticas, precio de monedas fiat y criptomonedas, búsqueda en
-					Wikipedia, juegos y comandos de ocio."
-					descriptioncolor = "text-black"
-					github = "https://www.github.com/chapig/alfonso"
-					githubcolor = "text-black"
-				/>
-
-				<Project
-					backgroundcolor = "bg-gray-100"
-					title="Base de datos para tutoría"
-					titlecolor = "text-black"
-					subtitle="Puente de bases de datos"
-					subtitlecolor = "text-gray-900"
-					tag="Python & SQLite"
-					tagcolor = "bg-gradient-to-r from-yellow-300 to-blue-500"
-					tagtextcolor = "text-black"
-					description="Puente para controlar datos de la base de datos, que contiene fecha de pago, monto, deuda, código de estudiante, nacionalidad y país de residencia."
-					descriptioncolor = "text-black"
-					github = "https://www.github.com/chapig/tutoringdb"
-					githubcolor = "text-black"
-				/>
-
-				<Project
-				backgroundcolor = "bg-gray-100"
-				title="Mathenize"
-				titlecolor = "text-black"
-				subtitle="Parser de operaciones matemáticas"
-				subtitlecolor = "text-gray-900"
-				tag="Julia"
-				tagcolor = "bg-pink-500"
-				tagtextcolor = "text-white"
-				description="Realiza operaciones matemáticas desde strings usando el lenguaje de
-				programación Julia, la mayoría de las funciones matemáticas y expresiones son
-				compatibles, incluyendo matrices y operaciones con paréntesis."
-				descriptioncolor = "text-black"
-				github = "https://www.github.com/chapig/mathenizejl"
-				githubcolor = "text-black"
-				/>
-
-				<Project
-					backgroundcolor = "bg-gray-100"
-					title="chapig.com"
-					titlecolor = "text-black"
-					subtitle="Sitio web"
-					subtitlecolor = "text-gray-900"
-					tag="Svelte"
-					tagcolor = "bg-red-700"
-					tagtextcolor = "text-white"
-					description="Sitio web que muestra el currículum de Luis C. Gómez, sus servicios y proyectos."
-					descriptioncolor = "text-black"
-					github = "https://www.github.com/chapig/website"
-					githubcolor = "text-black"
-				/>
-			</section>
-		</Route>
-
-		<Route path="cv">
+			<div in:fly>
+				<Services />
+			</div>
 		</Route>
 
 		<Route path="contact">
