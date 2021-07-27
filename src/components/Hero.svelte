a534f04430223a783d84f6130d095be910511c6e
Luis C. Gómez <46666572+chapig@users.noreply.github.com>
2021-07-26 21:05:55 -0400

    Update Hero.svelte

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,9 +1,12 @@
-<section class="pt-28 lg:pt-24 bg-white">
+<script>
+    import { Link } from "svelte-navigator";
+</script>
+<section class="pt-28 lg:pt-24">
     <div class="px-12 mx-auto max-w-7xl">
         <div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-            <h1 class="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
+            <p class="mb-8 text-4xl font-bold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
                 <span>Bienvenid@, soy</span>  <span class="lg:block md:block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-yellow-600 to-yellow-900">Luis C. Gómez</span>
-            </h1>
+            </p>
 
                 <div class="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
                     <div class="gap-6 md:grid-cols-2 lg:grid-cols-3">
@@ -16,14 +19,14 @@
                 </div>
 
             <div class="space-x-0 md:space-x-2 md:mb-8">
-                <a href="#_" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-black bg-yellow-400 rounded-md sm:w-auto sm:mb-0 hover:bg-gray-800 hover:text-white transition duration-200">
+                <Link to="cv" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg text-black bg-yellow-400 rounded-md sm:w-auto sm:mb-0 hover:bg-gray-800 hover:text-white transition duration-200">
                     Currículum
                     <svg class="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </a>
-                <a href="#_" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg bg-gray-100 rounded-md sm:w-auto sm:mb-0 hover:bg-gray-800 hover:text-white transition duration-200">
+                </Link>
+                <Link to="contact" class="inline-flex items-center justify-center w-full px-6 py-3 mb-2 text-lg bg-gray-100 rounded-md sm:w-auto sm:mb-0 hover:bg-gray-800 hover:text-white transition duration-200">
                     Contáctame
                     <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"></path></svg>
-                </a>
+                </Link>
             </div>
         </div>
     </div>
