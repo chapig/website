670272713963f948c6958807b9e68533d5d0f281
Luis C. Gómez <46666572+chapig@users.noreply.github.com>
2022-09-25 17:31:34 -0400

    Update Contact.svelte

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,15 +1,33 @@
 <section class="pt-24 w-full max-w-2xl px-6 py-4 mx-auto">
     <h2 class="text-6xl font-semibold text-center text-black dark:text-white">Ponte en contacto.</h2>
     <p class="mt-3 text-center text-black">Ya sea para clases de idiomas o servicios de software, marketing o diseño gráfico.</p>
+
+    <div class="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
+        <a href="#_" class="flex flex-col items-center px-4 py-3 text-gray-700 rounded-md">
+            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+            </svg>
+
+            <span class="mt-">Anzoátegui, Venezuela</span>
+        </a>
+
+        <a href="#_" class="flex flex-col items-center px-4 py-3 text-gray-700 rounded-md">
+            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+            </svg>
+
+            <span class="mt-2">+58 412-0881132</span>
+        </a>
+
         <a href="#_" class="flex flex-col items-center px-4 py-3 text-gray-700 rounded-md">
             <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                 <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
             </svg>
 
-            <span class="mt-2">luis@chapi.dev</span>
+            <span class="mt-2">developerchapi@gmail.com</span>
         </a>
     </div>
-    
+
 
 </section>
